6cee7df886a8137881560bbe5259113cbb7310b5
Kishore Kumar v <kishore.kumarkv93@gmail.com>
2026-02-23 17:17:53 +0530

    First commit

---
+++ src/langgraphagenticai/ui/uiconfigfile.py
@@ -0,0 +1,19 @@
+from configparser import ConfigParser
+
+class Config:
+    def __init__(self):
+        self.config_path='E:\DS_GenAI\3_AgenticAI\5.AgenticAi_with_Langgraph\src\langgraphagenticai\ui\uiconfigfile.ini'
+        self.config = ConfigParser()
+        self.config.read(self.config_path)
+
+    def get_llm_options(self):
+        return self.config['Default'].get("LLM_OPTIONS").split(", ")
+
+    def get_usecase_options(self):
+        return self.config["DEFAULT"].get("USECASE_OPTIONS").split(", ")
+
+    def get_groq_model_options(self):
+        return self.config["DEFAULT"].get("MODEL_OPTIONS").split(", ")
+
+    def get_page_title(self):
+        return self.config["DEFAULT"].get("PAGE_TITLE")    
